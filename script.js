add0bd6117291b2021b498d4ca9e53bcaf620be3
huseyinkaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2022-12-30 16:13:49 +0300

    fix

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ import { menuArray } from "./data.js";
 
 
 const popup = document.querySelector(".card-popup")
+const popupContainer = document.querySelector(".card-container")
+const submitForm = document.getElementById("formOne")
 const allInputs = document.querySelectorAll("input")
 const orderAmount = document.querySelector(".order-amount")
 const orderFinish = document.querySelector(".order-finish")
@@ -11,11 +13,61 @@ const menuSection = document.querySelector(".menu")
 
 document.addEventListener("submit", function (e) {
     if (e.target.id === "formOne") {
+        const submitFormData = new FormData(submitForm)
+        const name = submitFormData.get("fullName")
         e.preventDefault()
-        console.log("tikladi")
-        popup.classList.add("d-none")
-        orderAmount.classList.add("d-none")
-        orderFinish.classList.remove("d-none")
+
+        setTimeout(() => {
+            popupContainer.innerHTML = `
+
+            <div class="svgFile">
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="460" height="100" viewBox="0 0 120 30" fill="#000">
+                <circle cx="15" cy="15" r="15">
+                    <animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15"
+                        calcMode="linear" repeatCount="indefinite" />
+                    <animate attributeName="fill-opacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1"
+                        calcMode="linear" repeatCount="indefinite" />
+                </circle>
+                <circle cx="60" cy="15" r="9" fill-opacity="0.3">
+                    <animate attributeName="r" from="9" to="9" begin="0s" dur="0.8s" values="9;15;9"
+                        calcMode="linear" repeatCount="indefinite" />
+                    <animate attributeName="fill-opacity" from="0.5" to="0.5" begin="0s" dur="0.8s"
+                        values=".5;1;.5" calcMode="linear" repeatCount="indefinite" />
+                </circle>
+                <circle cx="105" cy="15" r="15">
+                    <animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15"
+                        calcMode="linear" repeatCount="indefinite" />
+                    <animate attributeName="fill-opacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1"
+                        calcMode="linear" repeatCount="indefinite" />
+                </circle>
+            </svg>
+            <p>
+            your order is being created, please wait
+            </p>
+        </div> 
+            
+            
+            `
+        }, 100);
+
+
+        setTimeout(() => {
+            popup.classList.add("d-none")
+            orderAmount.classList.add("d-none")
+            orderFinish.classList.remove("d-none")
+        }, 2000);
+
+
+        orderFinish.innerHTML = `
+        
+        <div class="order-finish">
+                <p>Thanks, ${name}! Your order is on its way!</p>
+            </div>
+        
+        
+        `
+
     }
 })
 
@@ -32,10 +84,7 @@ document.addEventListener("click", function (e) {
 
 
 
-    else if (e.target.id === "submitInBtn") {
 
-        submitBTN()
-    }
 
     else if (e.target.id === "close-btn") {
 
@@ -73,9 +122,7 @@ function completeBTN() {
 }
 
 
-function submitBTN() {
 
-}
 
 
 function closeBTN() {
@@ -206,3 +253,6 @@ function renderHTML() {
 }
 
 renderHTML()
+
+
+
